fb2922f9f35f95b46c21b25f403e64ca6eb2f835
Jonathan Alexander <me@jmalexan.com>
2022-05-30 13:32:46 -0700

    Add HSL player

--- app/src/main/java/club/osuapp/smartcam/LiveFragment.java
+++ app/src/main/java/club/osuapp/smartcam/LiveFragment.java
@@ -1,12 +1,35 @@
 package club.osuapp.smartcam;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import club.osuapp.smartcam.databinding.FragmentLiveBinding;
 
@@ -24,6 +47,22 @@ public class LiveFragment extends Fragment {
         return binding.getRoot();
 
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ExoPlayer player = new ExoPlayer.Builder(getContext()).build();
+
+        StyledPlayerView playerView = view.findViewById(R.id.live_player);
+        playerView.setPlayer(player);
+
+        /*MediaItem mediaItem = MediaItem.fromUri(videoUri);
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();*/
+    }
+
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/club/osuapp/smartcam/StatsFragment.java
+++ app/src/main/java/club/osuapp/smartcam/StatsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -30,5 +29,4 @@ public class StatsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
